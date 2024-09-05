6fa81157f8c6dec242e00192dc12dc0cf759ddc1
brunopaiva1 <brunopaivamaster10@gmail.com>
2024-09-05 15:29:05 -0300

    .

--- utils/utils.py
+++ utils/utils.py
@@ -10,12 +10,12 @@ from keras import applications, optimizers, Input
 from keras.models import Sequential, Model
 from keras.layers import Activation, Dropout, Flatten, Dense, Conv2D, MaxPooling2D
 from keras.utils import to_categorical
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score, precision_score, recall_score, f1_score
 from sklearn.utils.multiclass import unique_labels
 
-folder = '../MRI_Brain_Tumor/MRI_Brain_Tumor/Train'
+folder = '/home/brunopaiva/MRI_Brain_Tumor/MRI_Brain_Tumor/Train/'
 print(folder)
 
 image_width = 128
@@ -37,7 +37,7 @@ i = 0
 for mri in ['Glioma', 'Meningioma', 'No Tumor', 'Pituitary']:
     onlyfiles = [f for f in os.listdir(os.path.join(folder, mri + '/images'))if os.path.isfile(os.path.join(folder, mri + '/images', f))]
     for _file in onlyfiles:
-        img_path = os.path.join(folder, mri + 'images', _file)
+        img_path = os.path.join(folder, mri + '/images', _file)
         img = load_img(img_path, target_size=(image_height, image_width), color_mode='grayscale')
         x = img_to_array(img)
         dataset[i] = x
@@ -52,4 +52,31 @@ print("All images to array!")
 dataset = dataset.astype('float32')
 dataset /= 255
 
-classes = ['Raiva', 'Nojo', 'Medo', 'Felicidade', 'Neutro', 'Tristeza', 'Surpresa']
+classes = ['Glioma', 'Meningioma', 'No Tumor', 'Pituiary']
+
+first_image_index = {}
+
+for i, label in enumerate(y_dataset):
+    if label not in first_image_index:
+        first_image_index[label] = i
+
+num_classes = len(set(y_dataset))
+num_images_per_class = 1
+num_cols = num_classes
+num_rows = num_images_per_class
+
+fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, 5))
+
+for i in range(num_classes):
+    idx = first_image_index[i]
+
+    pixels = dataset[idx].reshape(image_height, image_width)
+
+    axes[i].imshow(pixels, cmap='gray')
+    axes[i].axis('off')
+
+    axes[i].set_title(f'{classes[i]}')
+
+# Exibe a figura
+plt.tight_layout()
+plt.show()
